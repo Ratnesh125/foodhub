4c06dc281134af8c436c0bfb1361852cafc67f62
Ratnesh125 <ratneshbharti125@gmail.com>
2023-09-03 20:07:42 +0530

    search bar

--- client/src/SearchBar.jsx
+++ client/src/SearchBar.jsx
@@ -1,7 +1,21 @@
+import search from "./assets/images/search.svg";
 export default function SearchBar() {
     return (
-        <div>
-            hii
+        <div className="p-52">
+            <div className="w-72 border-2 border-gray-300 h-10 rounded-full text-sm flex">
+                <input
+                    className="flex-grow h-full px-4 pl-8 rounded-l-full text-sm focus:outline-none"
+                    type="text"
+                    name="search"
+                    placeholder="Looking for something specific?"
+                />
+                <button
+                    type="submit"
+                    className="flex-shrink-0 bg-transparent bg-white border-none p-2 pr-4 pl-0 rounded-r-full"
+                >
+                    <img src={search} alt="Your SVG" />
+                </button>
+            </div>
         </div>
     )
 }
